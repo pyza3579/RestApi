03539890cad3cc30f222b29776a0ba51930db0d5
Iza Witkowska <isa.witkowska@gmail.com>
2020-09-09 21:04:59 +0200

    Prepared all for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./db.js');
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,15 +13,19 @@ const seatsRoutes = require('./routes/seats.routes');
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());  
-
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 app.use('/api', testimonialsRoutes); // add testimonials routes to server
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
 
-app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
-});
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+})
